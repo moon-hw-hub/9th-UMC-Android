f6d34c631d1ca75e3feb0b041b55cd77af7c6aee
princehw03 <princehw03@gmail.com>
2025-11-14 19:06:19 +0900

    [Week07] 엠키/문현우 미션 제출

--- week07/app/src/main/java/com/example/flo/locker/SavedalbumFragment.kt
+++ week07/app/src/main/java/com/example/flo/locker/SavedalbumFragment.kt
@@ -14,6 +14,7 @@ import com.google.gson.Gson
 
 class SavedalbumFragment : Fragment() {
 
+    //갱신
     lateinit var binding : FragmentSavedalbumBinding
     private var albumDatas = ArrayList<Album>()
 
